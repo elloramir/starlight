f6b33e55db25b858c32c62cc89766e1cf6310879
elloramir <cripboygit@gmail.com>
2023-02-07 20:38:20 -0300

    first rectangle

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,62 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <inttypes.h>
 
 int main(void)
 {
-    GLFWwindow* window;
+	GLFWwindow* window;
 
-    if (!glfwInit())
-        return -1;
+	if (!glfwInit())
+		return -1;
 
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
 
-    glfwMakeContextCurrent(window);
-    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+	glfwMakeContextCurrent(window);
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 
-    while (!glfwWindowShouldClose(window))
-    {
-    	glClearColor(0.3f, 0.1f, 0.0f, 1.f);
-        glClear(GL_COLOR_BUFFER_BIT);
+	uint32_t vao, vbo, ebo;
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+	float vertices[] = {
+		 0.5f,  0.5f, 0.0f,  // top right
+		 0.5f, -0.5f, 0.0f,  // bottom right
+		-0.5f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.5f, 0.0f   // top left 
+	};
 
-    glfwTerminate();
-    return 0;
+	uint32_t indices[] = {
+		0, 1, 3, // first triangle
+		1, 2, 3, // second triangle
+	};
+
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo);
+	glGenBuffers(1, &ebo);
+
+	glBindVertexArray(vao);
+	
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.3f, 0.1f, 0.0f, 1.f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
